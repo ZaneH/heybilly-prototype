ae6a0a91db98d2d630c39463786131822c5f1607
Zane Helton <me@zane.pw>
2024-01-24 15:14:31 -0500

    ft: update fine-tune for tool picker

--- src/ai/tool_picker.py
+++ src/ai/tool_picker.py
@@ -54,11 +54,14 @@ most appropriate to respond to this user. If the user asks to play music, use
 youtube, if they need a currency conversion, use wolfram_alpha, if they ask
 you to post a meme, use discord_post. If none of these tools match or are necessary
 for what the user says, simply choose no_tool. Your response will be in a JSON dict
-with only one key, "tool".
+with at least this key: "tool".
 
 You should only use wolfram_alpha for hard math problems or things that change
-(like weather). You know lots of simple facts already. Simple questions can be
-answered with no_tool. wolfram_alpha is very expensive for me.
+(e.g. weather, stock price, time, population). You know lots of simple facts
+already. Simple questions can be answered with no_tool.
+
+When choosing a discord_post, you can optionally include "query" and "text" keys
+to send a GIF and/or attach a message. This will help give others context.
 
 The tools you have available are:
 - no_tool
